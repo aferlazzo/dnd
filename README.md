73fb23dc9dbc2d665fa35ca8e3a33089971111e4
Anthony Ferlazzo <aferlazzo@gmail.com>
2016-08-11 14:15:46 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+#Drag-n-Drop   
+
+##Overview
+Hey guys! This is Tony Ferlazzo with my answer to your coding challenge. 
+
+You said:
+
+Create a webpage that displays a gallery of images, given a JSON array
+containing a list of image URLs.
+
+Here are the requirements:
+- Do not use any Javascript libraries or frameworks for your webpage.
+Javascript build tools are ok.
+- ES5 or above is preferred.
+- The basic layout should be a grid of pictures.
+- The images should be reorder-able by dragging and dropping.
+- Any other functionality is not expected.
+- The submitted code should be runnable and viewable. Add any
+instructions necessary to a README.md file.
+
+
+You are provided with a directory of sample pictures for convenience.
+
+Note: Initially there was no directory provided. Sent a quick email to the recruiter to see about obtaining it.
+ In the mean-time I'll use my own and get everything else set up.
+ 
+##My dev environment:
+ http server: 
+ 
+ &gt; npm install http-server -g
+ 
+ ##Usage
+ Starting http-server locally
+ 
+ &gt; cd project_directory
+
+ &gt; http-server
+ 
+ Now you can visit http://localhost:8080 to view your server
---
+++ test.js
@@ -0,0 +1,64 @@
+function dragStart(ev) {
+    ev.dataTransfer.effectAllowed='move';
+    ev.dataTransfer.setData("Text", ev.target.getAttribute('id'));
+    ev.dataTransfer.setDragImage(ev.target,0,0);
+
+    return true;
+}
+
+function dragEnter() {
+    event.preventDefault();
+    return true;
+}
+
+function dragOver(ev) {
+    return false;
+}
+
+function dragDrop(ev) {
+    var src = ev.dataTransfer.getData("Text");
+    ev.target.appendChild(document.getElementById(src));
+    ev.stopPropagation();
+    return false;
+}
+
+
+/**
+ *
+ * putImagesOnPage
+ *
+ * Without the aid of JavaScript libraries or frameworks read a JSON array of URLs of the images
+ * that will later be dragged and dropped.
+ *
+ */
+var putImagesOnPage =  {
+
+    putImageInDom: function(pathArray) {
+        var imageGallery = document.getElementsByClassName('picture');
+        var i;
+
+        for (i = 0; i < pathArray.length; i++) {
+            imageGallery[i].src = pathArray[i].image_path;
+        }
+    },
+
+    ajaxGet: function () {
+        var xhr = new XMLHttpRequest();
+        var url = "images.json";
+
+        //set up Ajax
+        xhr.onreadystatechange = function () {
+            var imagePaths;
+
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                imagePaths = JSON.parse(xhr.responseText);
+                putImagesOnPage.putImageInDom(imagePaths);
+            }
+        };
+        xhr.open("GET", url, true);
+        xhr.send();
+    }
+};
+
+this.putImagesOnPage.ajaxGet();
+
