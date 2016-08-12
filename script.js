9b51b7f55a3f6252c18976454ab85eaa774bfcc3
Anthony Ferlazzo <aferlazzo@gmail.com>
2016-08-11 17:53:27 -0700

    cleaned things up a bit

--- script.js
+++ script.js
@@ -36,34 +36,72 @@ function dragDrop(ev) {
  * that will later be dragged and dropped.
  *
  */
-var putImagesOnPage =  {
 
-    putImageInDom: function(pathArray) {
-        var imageGallery = document.getElementsByClassName('picture');
-        var i;
 
-        for (i = 0; i < pathArray.length; i++) {
-            imageGallery[i].src = pathArray[i].image_path;
-        }
-    },
 
-    ajaxGet: function () {
-        var xhr = new XMLHttpRequest();
-        var url = "images.json";
+    var putImagesOnPage = {
 
-        //set up Ajax
-        xhr.onreadystatechange = function () {
-            var imagePaths;
+        // Helper function to get an element's exact position
+        getPosition: function(el) {
+            var xPos = 0;
+            var yPos = 0;
 
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                imagePaths = JSON.parse(xhr.responseText);
-                putImagesOnPage.putImageInDom(imagePaths);
+            while (el) {
+                if (el.tagName == "BODY") {
+                    // deal with browser quirks with body/window/document and page scroll
+                    var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+                    var yScroll = el.scrollTop || document.documentElement.scrollTop;
+
+                    xPos += (el.offsetLeft - xScroll + el.clientLeft);
+                    yPos += (el.offsetTop - yScroll + el.clientTop);
+                } else {
+                    // for all other non-BODY elements
+                    xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+                    yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+                }
+
+                el = el.offsetParent;
             }
-        };
-        xhr.open("GET", url, true);
-        xhr.send();
-    }
-};
+            return {
+                x: xPos,
+                y: yPos
+            };
+        },
 
-this.putImagesOnPage.ajaxGet();
+
+        putImageInDom: function(pathArray) {
+            var imagesInDom = document.getElementsByClassName('picture');
+            var i;
+            var top;
+            var left;
+            var  current;
+
+            for (i = 0; i < pathArray.length; i++) {
+                imagesInDom[i].src = pathArray[i].image_path;
+
+                current = this.getPosition(document.getElementById(imagesInDom[i].id));
+                console.log(imagesInDom[i].id, " is located at top: ", current.y, " left: ", current.x);
+            }
+        },
+
+        ajaxGet: function () {
+            var xhr = new XMLHttpRequest();
+            var url = "images.json";
+
+            //set up Ajax
+            xhr.onreadystatechange = function () {
+                var imagePaths;
+
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    imagePaths = JSON.parse(xhr.responseText);
+                    putImagesOnPage.putImageInDom(imagePaths);
+                }
+            };
+            xhr.open("GET", url, true);
+            xhr.send();
+        }
+    };
+
+    putImagesOnPage.ajaxGet();
+
 
